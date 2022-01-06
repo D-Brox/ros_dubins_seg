8741abbd18fd7a24a7d5b87905e3bf3c4562e592
Arthur <arthurnunes.petee@gmail.com>
2022-01-06 18:11:46 -0300

    send/recieve memory

--- scripts/ros_comm_node.py
+++ scripts/ros_comm_node.py
@@ -1,61 +1,44 @@
 #!/usr/bin/env python
-from __future__ import print_function
+
+import sys
+import time
+
 import rospy
 from nav_msgs.msg import Odometry
-from math import sqrt, atan2, exp, atan, cos, sin, acos, pi, asin, atan2
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
-from time import sleep
-import tf
+from tf.transformations import euler_from_quaternion
+
 from dubins_seg.srv import *
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
-class CommunicationManager():
-    def __init__(self, N_robots, groups, M_groups,freq=20):
-        self.N_robots = N_robots
-        self.freq = float(freq)
+class CommNode():
 
-        self.Rb = 0
-        self.d = 0
-        self.c = 0
-        self.ref_vel = 0
-        self.x = [0]*self.N_robots
-        self.y = [0]*self.N_robots
-        self.theta = [0]*self.N_robots
-        
+    def __init__(self, n_robots, freq=20):
+        self.__n_robots = n_robots
+        self.__freq = float(freq)
+        self.__x = [0]*self.__n_robots
+        self.__y = [0]*self.__n_robots
+        self.__theta = [0]*self.__n_robots
+        self.__params = self.load_sim_param()      
         # Init node
-        rospy.init_node('communication_manager')
+        rospy.init_node('communication')
         # Topics
-        for robot_number in range(self.N_robots):
-            msg = 'Subscribing to ' + 'robot_' + str(robot_number) + '/base_pose_ground_truth'
-            rospy.loginfo(msg)
+        for robot_number in range(self.__n_robots):
             rospy.Subscriber('robot_' + str(robot_number) + '/base_pose_ground_truth', Odometry, self.callback_pose, (robot_number))
-        self.rate = rospy.Rate(self.freq)
-
-        
+        self.__rate = rospy.Rate(self.__freq)
 
     def main_loop(self):
+        time.sleep(1)
         while not rospy.is_shutdown():
-            self.load_sim_param()
-            for i in range(self.N_robots):
-                for j in range(i+1,self.N_robots):
-                    j_is_in_c_radius = (self.x[i] - self.x[j])**2 + (self.y[i] - self.y[j])**2 < self.c**2
-                    if j_is_in_c_radius:
+            self.__params = self.load_sim_param()
+            for i in range(self.__n_robots):
+                for j in range(i+1,self.__n_robots):
+                    ij_communicate =  (self.__x[i] - self.__x[j])**2 + (self.__y[i] - self.__y[j])**2 < self.__params['c']**2
+                    if ij_communicate:
                         try:
                             send_memory_i = rospy.ServiceProxy('/robot_' + str(i) + '/send_mem_' + str(i),send_memory)
                             send_memory_j = rospy.ServiceProxy('/robot_' + str(j) + '/send_mem_' + str(j),send_memory)
                             receive_memory_i = rospy.ServiceProxy('/robot_' + str(i) + '/' + 'receive_mem_' + str(i), receive_memory)
                             receive_memory_j = rospy.ServiceProxy('/robot_' + str(j) + '/' + 'receive_mem_' + str(j), receive_memory)
-
                             memory_i = send_memory_i(i,j)
                             memory_j = send_memory_j(j,i)
                             resp_i_recv_j = receive_memory_i(memory_j.curve_index, memory_j.group, memory_j.time_curve, memory_j.time, memory_j.pose2D_x, memory_j.pose2D_y, memory_j.pose2D_theta, memory_j.mov_will, memory_j.number)
@@ -65,31 +48,33 @@ class CommunicationManager():
 
     def load_sim_param(self):
         # Load simulation parameters
-        self.Rb = float(rospy.get_param('/Rb'))
-        self.d = float(rospy.get_param('/d'))
-        self.c = float(rospy.get_param('/c'))
-        self.ref_vel = float(rospy.get_param('/ref_vel'))
+        params = {
+            'Rb': float(rospy.get_param('/Rb')),
+            'd': float(rospy.get_param('/d')),
+            'c': float(rospy.get_param('/c')),
+            'ref_vel': float(rospy.get_param('/ref_vel'))
+        }
+        return params
 
     def callback_pose(self,data,robot_number):
-        self.x[robot_number] = data.pose.pose.position.x
-        self.y[robot_number] = data.pose.pose.position.y
+        self.__x[robot_number] = data.pose.pose.position.x
+        self.__y[robot_number] = data.pose.pose.position.y
         x_q = data.pose.pose.orientation.x
         y_q = data.pose.pose.orientation.y
         z_q = data.pose.pose.orientation.z
         w_q = data.pose.pose.orientation.w
         euler = euler_from_quaternion([x_q, y_q, z_q, w_q])
-        self.theta[robot_number] = euler[2]
+        self.__theta[robot_number] = euler[2]
+
 
 if __name__ == '__main__':
     try:
-        N_robots = int(len(sys.argv)) - 3
+        n_robots = int(len(sys.argv)) - 3
         groups = []
-        for aux in range(1, N_robots+1):
+        for aux in range(1, n_robots+1):
             groups.append(int(sys.argv[aux]))
         M_groups = max(groups)+1
-
-        communication_manager = CommunicationManager(N_robots, groups, M_groups)
-
-        communication_manager.main_loop()
+        comm_node = CommNode(n_robots)
+        comm_node.main_loop()
     except rospy.ROSInterruptException:
         pass
--- scripts/ros_dubin_node.py
+++ scripts/ros_dubin_node.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import print_function
+
 import sys
 import time
 
@@ -7,9 +7,9 @@ import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from rosgraph_msgs.msg import Clock
-from sensor_msgs.msg import LaserScan
+# from sensor_msgs.msg import LaserScan
 from tf.transformations import euler_from_quaternion
-from visualization_msgs.msg import Marker, MarkerArray
+# from visualization_msgs.msg import Marker, MarkerArray
 
 from pydubinsseg.segregationcontrol import SegregationControl
 from pydubinsseg import state
@@ -31,8 +31,8 @@ class ControlNode():
         rospy.Subscriber('/clock', Clock, self.callback_time)
         self.__rate = rospy.Rate(self.__freq)
         # # Services
-        # send_memory_obj = rospy.Service('send_mem_' + str(self.dubin.get_number()), send_memory, self.dubin.send_memory)
-        # receive_memory_obj = rospy.Service('receive_mem_' + str(self.dubin.get_number()), receive_memory, self.dubin.receive_memory)
+        send_memory_obj = rospy.Service('send_mem_' + str(self.__robot_number), send_memory, self.send_memory)
+        receive_memory_obj = rospy.Service('receive_mem_' + str(self.__robot_number), receive_memory, self.receive_memory)
         # Pub blank data to start
         self.pub_vel(0,0)
 
@@ -71,6 +71,45 @@ class ControlNode():
         vel.angular.z = w
         self.__publisher.publish(vel)
         self.__rate.sleep()
+
+    def send_memory(self,req):
+        curve_index_list = []
+        group_list = []
+        time_curve_list = []
+        time_list = []
+        pose2D_x_list = []
+        pose2D_y_list = []
+        pose2D_theta_list = []
+        will_list = []
+        number_list = []
+        for item in self.__segregation.send_memory():
+            curve_index_list.append( item['curve_index'] )
+            group_list.append( item['group'] )
+            time_curve_list.append( item['time_curve'] )
+            time_list.append( item['time'] )
+            pose2D_x_list.append( item['pose2D'][0] )
+            pose2D_y_list.append( item['pose2D'][1] )
+            pose2D_theta_list.append( item['pose2D'][2] )
+            will_list.append( item['will'] )
+            number_list.append( item['number'] )
+        return send_memoryResponse(curve_index_list, group_list, time_curve_list, time_list, pose2D_x_list, pose2D_y_list, pose2D_theta_list, will_list, number_list)
+
+    def receive_memory(self,req):
+        j_memory_data_list = []
+        n_items = len(req.curve_index)
+        for index in range(n_items):
+            item = {
+                'group': req.group[index],
+                'number': req.curve_index[index],
+                'curve_index': req.curve_index[index],
+                'time_curve': req.time_curve[index],
+                'time': req.time[index],
+                'pose2D': [req.pose2D_x[index], req.pose2D_y[index], req.pose2D_theta[index]],
+                'will': req.mov_will[index]
+            }
+            j_memory_data_list.append(item)
+        self.__segregation.recieve_memory(j_memory_data)
+        return receive_memoryResponse(0,0)  
 
     def callback_pose(self, data):
         x = data.pose.pose.position.x
