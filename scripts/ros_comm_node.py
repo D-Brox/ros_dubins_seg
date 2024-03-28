71c8189f989aac5c7187f150ef9482f28e702872
David Brochero <davidfbg00@gmail.com>
2024-03-28 02:17:24 +0000

    Bump submodule

--- scripts/ros_comm_node.py
+++ scripts/ros_comm_node.py
@@ -13,7 +13,7 @@ from dubins_seg.srv import *
 
 class CommNode():
 
-    def __init__(self, n_robots, freq=20):
+    def __init__(self, n_robots, freq=120):
         self.__n_robots = n_robots
         self.__freq = float(freq)
         self.__x = [0]*self.__n_robots
